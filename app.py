76965dd440b098150c9940f28b5b83e2d76cf0b9
Stephanie Burgos <burgoss@google.com>
2023-03-11 14:37:41 -0500

    Updating and deleting data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -9,7 +9,22 @@ def index():
 
     return render_template('index.html', engineers = engineers)
 
-@app.route('/success', methods=['POST'])
+@app.route('/edit/<rowid>')
+def edit(rowid):
+    engineer = get_engineer(rowid)
+    return render_template('edit.html', engineer = engineer)
+
+@app.route('/edit-engineer/<rowid>', methods=['POST'])
+def edit_engineer(rowid):
+    name = request.form['name']
+    age = int(request.form['age']) 
+    height = request.form['height']
+    group = request.form['group']
+    update_engineer(name, age, height, group, rowid)
+
+    return redirect(url_for('index'))
+
+@app.route('/post-engineer', methods=['POST'])
 def submit():
     name = request.form['name']
     age = int(request.form['age']) 
@@ -17,7 +32,12 @@ def submit():
     group = request.form['group']
     add_engineer(name, age, height, group)
 
-    return render_template('success.html')
+    return redirect(url_for('index'))
+
+@app.route('/delete-engineer/<rowid>')
+def delete(rowid):
+    delete_engineer(rowid)
+    return redirect(url_for('index'))
 
 def add_engineer(name, age, height, group):
     conn = sqlite3.connect('./static/data/team-edge.db')
@@ -26,18 +46,51 @@ def add_engineer(name, age, height, group):
     conn.commit()
     conn.close()
 
+def update_engineer(name, age, height, group, rowid):
+    conn = sqlite3.connect('./static/data/team-edge.db')
+    curs = conn.cursor()
+    curs.execute("UPDATE engineers SET name = ?, age = ?, height = ?, group_num = ? WHERE rowid = ?", (name, age, height, group, rowid))
+    conn.commit()
+    conn.close()
+
+def delete_engineer(rowid):
+    conn = sqlite3.connect('./static/data/team-edge.db')
+    curs = conn.cursor()
+    curs.execute("DELETE FROM engineers WHERE rowid = ?", (rowid,))
+    conn.commit()
+    conn.close()
+
+def get_engineer(rowid):
+    conn = sqlite3.connect('./static/data/team-edge.db')
+    curs = conn.cursor()
+    result = curs.execute("SELECT rowid, * FROM engineers WHERE rowid = ?", (rowid,))
+    engineer = {}
+
+    for row in result: 
+        engineer = {
+            'rowid': row[0],
+            'name': row[1],
+            'age': row[2],
+            'height': row[3],
+            'group': row[4]
+        }
+
+    conn.close()
+    return engineer
+
 def get_all_engineers():
     conn = sqlite3.connect('./static/data/team-edge.db')
     curs = conn.cursor()
-    result = curs.execute("SELECT * FROM engineers")
+    result = curs.execute("SELECT rowid, * FROM engineers")
     engineers = []
 
     for row in result: 
         engineer = {
-            'name': row[0],
-            'age': row[1],
-            'height': row[2],
-            'group': row[3]
+            'rowid': row[0],
+            'name': row[1],
+            'age': row[2],
+            'height': row[3],
+            'group': row[4]
         }
         engineers.append(engineer)
 
